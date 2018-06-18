5a7296c5f299ea479d90d34b564cdf968ed19a7a
aleckwhite <alexander.k.white@gmail.com>
2018-06-18 15:09:17 -0400

    now can upload to S3

--- Best Friend Match/CameraViewController.swift
+++ Best Friend Match/CameraViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import AWSCore
+import AWSMobileClient
+import AWSS3
 import AVFoundation
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let mediaHelper = MediaHelper()
     
     @IBOutlet var imageView: UIImageView!
     
@@ -22,9 +27,13 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func uploadImageButtonTapped(_ sender: UIButton) {
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.6) as Data?
+        mediaHelper.uploadMedia(mediaData: imageData!, mediaID: "mobileTest")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         imagePicker.delegate = self
     }
     
@@ -34,16 +43,65 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .scaleAspectFit
             imageView.image = pickedImage
-            print("set new image")
         }
-        else{
-            print("wouldnt let me pick it")
-        }
-        
         dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
+}
+
+class MediaHelper {
+    
+    var transferManager: AWSS3TransferManager!
+    
+    init() {
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType:AWSRegionType.USEast2,
+                                                                identityPoolId:"us-east-2:a28a038b-8d56-4a9a-bf44-06b7daa67308")
+        let configuration = AWSServiceConfiguration(region:AWSRegionType.USEast2, credentialsProvider:credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        self.transferManager = AWSS3TransferManager.default()
+    }
+    
+    func uploadMedia(mediaData: Data, mediaID: String) {
+        
+        let uploadingFileURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(mediaID)
+        do {
+            try mediaData.write(to: uploadingFileURL, options: Data.WritingOptions.atomic)
+        } catch let error as NSError {
+            print("Could not save \(error), \(error.userInfo)")
+        }
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()!
+        
+        uploadRequest.bucket = "bestfriendpets"
+        uploadRequest.key = mediaID + ".jpg"
+        uploadRequest.body = uploadingFileURL
+        uploadRequest.contentType = "image/jpeg"
+        transferManager.upload(uploadRequest).continueWith(executor: AWSExecutor.mainThread(), block: { (task:AWSTask<AnyObject>) -> Any? in
+            
+            if let error = task.error as NSError? {
+                if error.domain == AWSS3TransferManagerErrorDomain, let code = AWSS3TransferManagerErrorType(rawValue: error.code) {
+                    switch code {
+                    case .cancelled, .paused:
+                        break
+                    default:
+                        print("Error uploading: \(uploadRequest.key!) Error: \(error)")
+                    }
+                } else {
+                    print("Error uploading: \(uploadRequest.key!) Error: \(error)")
+                }
+                return nil
+            }
+            
+            let uploadOutput = task.result
+            print("Upload complete for: \(uploadRequest.key!)")
+            return nil
+        })
+    }
+    
 }
