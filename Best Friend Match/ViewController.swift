01c24f09f50bc10d370db1bed8aaa4ddc10489bd
aleckwhite <alexander.k.white@gmail.com>
2018-06-18 10:05:27 -0400

    Initial Commit

---
+++ Best Friend Match/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Best Friend Match
+//
+//  Created by Alexander K. White on 6/18/18.
+//  Copyright © 2018 Alexander K. White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
